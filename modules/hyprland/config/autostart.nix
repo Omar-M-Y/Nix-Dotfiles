051d28f93676bfc314571c6a9dc29e7a920538fb
Omar-M-Y <yahya.omar_@outlook.com>
2026-01-04 16:54:29 +0000

    Removed Ambxst

--- modules/hyprland/config/autostart.nix
+++ modules/hyprland/config/autostart.nix
@@ -2,12 +2,11 @@
     imports = [ ./keybinds.nix];
   wayland.windowManager.hyprland.settings = {
         exec-once = [
-            # "swww-daemon"
+            "swww-daemon"
             "discord --start-minimized"
             "steam -silent"
-            # "matugen"
-            # "ambxst"
-            # "quickshell"
+            "matugen"
+            "quickshell"
             # "$terminal"
         ];
   };
