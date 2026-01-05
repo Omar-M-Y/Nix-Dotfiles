a664f65bb15a0d1d96d88fcf6d15529c5e412e36
Omar-M-Y <yahya.omar_@outlook.com>
2026-01-05 19:14:31 +0000

    Updated Waybar

--- modules/scripts/default.nix
+++ modules/scripts/default.nix
@@ -7,8 +7,17 @@ let
       exit 1
     fi
 
+    # 1. Cache the wallpaper for Hyprlock
+    # This copies the image to ~/.current_wallpaper so hyprlock can always find it
+    cp -f "$1" "${config.home.homeDirectory}/.current_wallpaper"
+
+    # 2. Set the wallpaper with swww
     swww img "$1" --transition-type grow --transition-pos 0.8,0.9 --transition-step 90 --transition-fps 60
+    
+    # 3. Generate colors with Matugen
     matugen image "$1"
+    
+    # 4. Reload apps to pick up new colors
     ${pkgs.procps}/bin/pkill -SIGUSR1 kitty
     ${pkgs.procps}/bin/pkill -SIGUSR2 waybar
     ${pkgs.hyprland}/bin/hyprctl reload
