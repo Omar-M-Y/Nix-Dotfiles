c1744262e5ba9713a4b1ceb978bced80ccc21e42
Omar-M-Y <yahya.omar_@outlook.com>
2026-01-05 14:11:09 +0000

    Fix sddm

--- configuration.nix
+++ configuration.nix
@@ -246,6 +246,7 @@ programs = {
         gamescopeSession = {
             enable = true;
           };
+    };
     fish = {
         enable = true;
     };
@@ -398,5 +399,4 @@ fileSystems = {
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.11"; # Did you read the comment?
 
-  };
 }
