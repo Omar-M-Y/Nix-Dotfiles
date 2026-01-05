e55eb19b5b390670c1284e3f0fd16d48bcfbf881
Omar-M-Y <yahya.omar_@outlook.com>
2026-01-05 17:44:23 +0000

    Waybar

--- modules/waybar/config.nix
+++ modules/waybar/config.nix
@@ -1,8 +1,15 @@
 {
   mainBar = {
-    layer = "top";
+    layer = "bottom";
     position = "top";
     height = 30;
+    margin-left = 0;
+    margin-bottom = 0;
+    margin-right = 0;
+    spacing = 5;
+
+    // builtins.import ./modules
+
     # modules-left = ;
     # modules-center = ;
     # modules-right= ;
---
+++ modules/waybar/modules/default.nix
@@ -0,0 +1,35 @@
+{
+    "hyprland/window" = {
+      max-length = 100;
+      separate-output = false;
+      };
+
+    "hyprland/workspaces" = {
+        all-outputs = true;
+        format = "{name}";
+        format-icons = {
+            "1" = "";
+            "2" = "";
+            "3" = "";
+            "4" = "";
+            "5" = "";
+            "6" = "";
+            "7" = "";
+            "8" = "";
+            "9" = "";
+            "10" = "";
+          };
+          sort-by-number = true;
+          all-outputs = false;
+          active-only = false;
+          on-click = "activate";
+      };
+
+      "tray" = {
+          icon-size = 16;
+          spacing = 8;
+          show-passive-items = true;
+        };
+
+    
+  }
